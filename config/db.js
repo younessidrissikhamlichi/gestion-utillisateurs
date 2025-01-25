3ee1ef1d9c824c24c0ae58c987b6e1f335c92399
fullstack-khamlichi <114249953+fullstack-khamlichi@users.noreply.github.com>
2025-01-25 21:42:05 +0100

    3ème commit: mise à jour du projet: ajouter la partie prod

--- config/db.js
+++ config/db.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+require("dotenv").config(); // Charger les variables d'environment
+
+process.env.NODE_ENV = process.env.NODE_ENV || "developpement";
+
+const mongoURI = process.env.NODE_ENV === "production" 
+  ? process.env.MONGO_URI_PROD
+  : process.env.MONGO_URI_LOCAL;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(mongoURI);
     console.log("Connexion à MongoDB réussie");
   } catch (e) {
     console.error("Erreur de connexion à MongoDB: ", e.message);
